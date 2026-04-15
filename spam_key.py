67d75a9f19bce24bc172d3f5ca1e446353b338df
etcern <115371229+etcern@users.noreply.github.com>
2026-04-15 13:44:14 +0200

    Adding my first GitHub Script
    A script that allows the user to give an input state from a keyboard keypress. 
    <h1>The reason </h1>
    Why I started making this script, was just for the sake of spamming a key, without installing or using any other programms, but my own knowledge. I was playing a minecraft modded modpack (All The Mods 10) and I had to maximize the level of a spefic relic by spammign the shift key. I decided building a simple script using pyautogui, but it seemed to not be working. On this journey I hjave found out that it was not running on the level it was supposed to run, in terms of input rules, so I ran into a rabbit hole and found out pynput. On the way I found out that it runs on the same level as the WindowsAPI so here I am, building my own tool to automatize certain processes inside games, among bunch of other stuff that comes to my crazy mind.

---
+++ spam_key.py
@@ -0,0 +1,45 @@
+from pynput.keyboard import Key, Controller, Listener 
+import time
+import threading
+
+# pyinput is a library that allows you to control and monitor input devices. 
+# In this case, we are using it to control the keyboard. It runs on the same lever as the 
+# Windows API, so it can be used to simulate key presses and releases reliably. 
+# Good for game scripting macros :) pyautogui is trash lol
+
+
+keyboard = Controller()
+time.sleep(3)
+
+# Define gloval alive variable to control macro thread
+alive = False
+
+def pressKey():
+    while True:
+        if alive:
+            keyboard.press(Key.shift)
+            time.sleep(0.05)
+            keyboard.release(Key.shift)
+            time.sleep(0.05)
+        else:
+            time.sleep(0.1)
+
+def on_press(key):
+    global alive
+
+    # Toggle with key  (F8 in this case to start/stop the macro)
+    if key == Key.f8:
+        alive = not alive
+
+
+# Threading that make sma programmo run in backgrou8ndo  :D daemon is like a background thread that will 
+# automatically close when the main program exits. It s normal, since programms use this all 
+# the time to run background tasks without blocking the main thread. Once the programm 
+# stopps, the daemon thread will also stop, so you dont have to worry about it running forever.
+macro_thread = threading.Thread(target = pressKey, daemon=True)
+macro_thread.start()
+
+
+# Listener is a class from the pynput library that allows you to listen for keyboard events before the thread function is executed.
+with Listener(on_press=on_press) as listener:
+    listener.join()
